edf8a532f28edddd02b74784e882cd9ca741302c
leoshi <leoshi@leoshideMac-mini-2.local>
2018-03-29 17:33:48 +0800

    yinfo
    柱形图与折线图混合动画

--- test11/YFChartView.m
+++ test11/YFChartView.m
@@ -88,6 +88,47 @@
     
 }
 
+//渐变柱形图
+- (void)drawRectWithAnimate{
+    
+    CAShapeLayer *rectLayer = [CAShapeLayer layer];
+    rectLayer.frame = self.bounds;
+    rectLayer.strokeColor = [UIColor colorWithRed:222/255.0 green:79/255.0 blue:70/255.0 alpha:1.0].CGColor;
+    rectLayer.fillColor = [UIColor clearColor].CGColor;
+    rectLayer.lineWidth = content_width/10-15;
+    rectLayer.lineCap = kCALineCapSquare;//kCALineCapRound 线的头部为圆
+    rectLayer.lineJoin = kCALineJoinMiter;
+    
+    CAGradientLayer *gradLayer = [CAGradientLayer layer];
+    gradLayer.frame = rectLayer.frame;
+    gradLayer.colors = [NSMutableArray arrayWithArray:@[(__bridge id)[UIColor colorWithRed:253 / 255.0 green:164 / 255.0 blue:8 / 255.0 alpha:1.0].CGColor, (__bridge id)[UIColor colorWithRed:251 / 255.0 green:37 / 255.0 blue:45 / 255.0 alpha:1.0].CGColor]];
+    gradLayer.locations = @[@(0.3),@(0.5)];
+    gradLayer.startPoint = CGPointMake(0, 1);
+    gradLayer.endPoint = CGPointMake(1, 0);
+    [gradLayer setMask:rectLayer];
+    [self.layer addSublayer:gradLayer];
+    
+    UIBezierPath    *rectPath = [UIBezierPath bezierPath];
+    for (int i = 0; i < _numArr.count; i++) {
+        CGFloat x_point = blank+(content_width/10-2)*(i+1);
+        NSInteger num = [[_numArr objectAtIndex:i] intValue];
+        CGFloat y_real = height-blank-(num*(content_height/max_value));
+        [[NSString stringWithFormat:@"%ld",(long)num] drawAtPoint:CGPointMake(x_point-10, y_real-35) withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:11]}];
+        [rectPath moveToPoint:CGPointMake(x_point, height-blank)];
+        [rectPath addLineToPoint:CGPointMake(x_point, y_real)];
+    }
+    rectLayer.path = rectPath.CGPath;
+    
+    CABasicAnimation *animate = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+    animate.fromValue = @(0.0);
+    animate.toValue = @(1.0);
+    animate.duration = 3.0;
+    animate.autoreverses = NO;
+    [rectLayer addAnimation:animate forKey:@"strokeEnd"];
+    
+    
+}
+//渐变折线图
 - (void)drawLineWithAnimate{
     
     CAShapeLayer *lineLayer = [[CAShapeLayer alloc]init];
@@ -100,7 +141,7 @@
     //渐变遮罩
     CAGradientLayer *gradLayer = [CAGradientLayer layer];
     gradLayer.frame = lineLayer.frame;
-    gradLayer.colors = [NSMutableArray arrayWithArray:@[(__bridge id)[UIColor colorWithRed:253 / 255.0 green:164 / 255.0 blue:8 / 255.0 alpha:1.0].CGColor, (__bridge id)[UIColor colorWithRed:251 / 255.0 green:37 / 255.0 blue:45 / 255.0 alpha:1.0].CGColor]];
+    gradLayer.colors = [NSMutableArray arrayWithArray:@[(__bridge id)[UIColor colorWithRed:153 / 255.0 green:134 / 255.0 blue:8 / 255.0 alpha:1.0].CGColor, (__bridge id)[UIColor colorWithRed:51 / 255.0 green:37 / 255.0 blue:45 / 255.0 alpha:1.0].CGColor]];
     gradLayer.locations = @[@(0.3),@(0.5)];
     
     gradLayer.startPoint = CGPointMake(0, 1);
@@ -114,7 +155,7 @@
         CGFloat x_point = blank+(content_width/10-2)*(i+1);
         NSInteger num = [[_numArr objectAtIndex:i] intValue];
         CGFloat y_real = height-blank-(num*(content_height/max_value));
-        [[NSString stringWithFormat:@"%ld",(long)num] drawAtPoint:CGPointMake(x_point-8, y_real-8) withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:14]}];
+//        [[NSString stringWithFormat:@"%ld",(long)num] drawAtPoint:CGPointMake(x_point-8, y_real-8) withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:14]}];
         if (i == 0) {
             [linePath moveToPoint:CGPointMake(x_point, y_real)];
         }
@@ -127,7 +168,7 @@
     CABasicAnimation *animate = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
     animate.fromValue = @(0.0);
     animate.toValue = @(1.0);
-    animate.duration = 3.0;
+    animate.duration = 5.0;
     animate.autoreverses = NO;
     [lineLayer addAnimation:animate forKey:@"strokeEnd"];
     
@@ -139,7 +180,7 @@
     [self drawBackgroundWithRect:rect];
     [self drawDish];
     [self drawLineWithAnimate];
-    
+    [self drawRectWithAnimate];
    
 }
 
